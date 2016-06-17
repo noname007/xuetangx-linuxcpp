948c8a5610d9a578c2a590009d6d5804ff7cb87f
yangzz <1479418380@qq.com>
2016-06-17 13:57:41 +0800

    14.04

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ extern "C"{
 #include "pthread.h"
 
 #include "unistd.h"
+#include "math.h"
 }
 
 
@@ -25,11 +26,65 @@ void * printBig(void * data){
     return  NULL;
 }
 
+
+//=============== 4
+void * IsPrime(void *n){
+    unsigned int p = reinterpret_cast<unsigned long  int > (n);
+    unsigned int i = 3;
+    unsigned int t = (uint)sqrt(p) + 1;
+
+    if(p == 2)
+    {
+        return reinterpret_cast<void *> (true);
+    }
+
+    if(p % 2 == 0)
+    {
+        return reinterpret_cast<void *> (false);
+    }
+
+
+    while (i <= t)
+    {
+        if(p % i == 0)
+        {
+            return reinterpret_cast<void *> (false);
+        }
+
+        i += 2;
+    }
+    return reinterpret_cast<void *>(true);
+}
+
 int main() {
-    cout << "Hello, World!" << endl;
-    pthread_t pid;
-    pthread_create(&pid,NULL,printsmall,NULL);
+    //cout << "Hello, World!" << endl;
+   // pthread_t pid;
+    //pthread_create(&pid,NULL,printsmall,NULL);
    // printf("thread id:%d",pid);
-    printBig(NULL);
+   // printBig(NULL);
+
+//    printf("%d %d\n", sizeof(int),sizeof(void *));
+    printf("%lu %lu\n", sizeof(int),sizeof(void *));
+
+    pthread_t tids[8];
+
+    bool primalities[8];
+
+    for(int i = 0; i<8;++i){
+        pthread_create(&tids[i],NULL,IsPrime, reinterpret_cast<void *>(i + 2));
+    }
+
+    for(int i = 0; i < 8;++i)
+    {
+        pthread_join(tids[i], reinterpret_cast<void **>(&primalities[i]));
+    }
+
+    for (int j = 0; j < 8; ++j) {
+        cout<<primalities[j]<<" ";
+    }
+
+    cout<<endl;
+
+
     return 0;
 }
